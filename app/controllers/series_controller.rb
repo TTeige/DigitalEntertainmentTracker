c5df9d5f8b3a09d9ebb5226541f13105eb44f565
Tim <tim.teige91@gmail.com>
2015-02-13 16:33:36 +0100

    Added a search bar in the nav bar and made so that the show view displays the xml file from tvdb

--- app/controllers/series_controller.rb
+++ app/controllers/series_controller.rb
@@ -1,2 +1,15 @@
+require 'tvdb'
+
 class SeriesController < ApplicationController
+
+	def create
+
+	end
+
+
+	def show
+		client = TVdb::Client.new(ENV['TVDB_API_KEY'])
+		@results = client.search('The Big Bang Theory')			
+	end
+
 end

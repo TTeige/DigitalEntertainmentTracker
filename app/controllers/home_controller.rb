575a643bdc62f0d53abfa8f1082ce4d03c23d723
Peter Munch-Ellingsen <peterme@peterme.net>
2015-02-21 23:14:51 +0100

    Reverted to working master branch. Merged in code complient with old TvDB gem by mistake.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,11 @@
 class HomeController < ApplicationController
   def index
+<<<<<<< HEAD
   end
 
   def about
 
+=======
+>>>>>>> parent of 9ae75d0... Added basic list of shows, sorted by day (alphabetically) and air-time (random formatting).
   end
 end

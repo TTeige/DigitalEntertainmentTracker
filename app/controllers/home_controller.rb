9ae75d0612c4e955ec306c3db4697126074f81eb
Peter Munch-Ellingsen <peterme@peterme.net>
2015-02-21 22:46:50 +0100

    Added basic list of shows, sorted by day (alphabetically) and air-time (random formatting).

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,27 @@
 class HomeController < ApplicationController
   def index
+    client = TVdb::Client.new(ENV['TVDB_API_KEY'])
+    # Should look in local database of popular shows for shows to display
+    results = client.search('The')# + client.search('Firefly')
+    # Should be list of shows
+    @shows = results.sort {|x,y|
+      
+      #elsif x.airs_dayofweek == "Daily"
+      #  -1
+      #elsif y.airs_dayofweek == "Daily"
+      #  1
+      if x.airs_dayofweek == y.airs_dayofweek
+        x.seriesname <=> y.seriesname
+      elsif x.airs_dayofweek.empty?
+        1
+      elsif y.airs_dayofweek.empty?
+        -1
+      else
+        case x.airs_dayofweek
+        when "Monday"
+          x_num=1
+        x.airs_dayofweek <=> y.airs_dayofweek
+      end
+      }
   end
 end

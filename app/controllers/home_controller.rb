bdef3b1f6cbd3701c0cd37aa83cfe7b86c38f2bd
Peter Munch-Ellingsen <peterme@peterme.net>
2015-02-21 23:21:26 +0100

    Fixed various bugs from improper revert. Thing should be back in working order.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,7 @@
 class HomeController < ApplicationController
   def index
-<<<<<<< HEAD
   end
 
   def about
-
-=======
->>>>>>> parent of 9ae75d0... Added basic list of shows, sorted by day (alphabetically) and air-time (random formatting).
   end
 end

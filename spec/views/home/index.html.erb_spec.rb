68495a53fb2bd76683749bc41d88e127ec28bba0
Peter Munch-Ellingsen <peterme@peterme.net>
2015-02-24 00:22:24 +0100

    Wrote simple test for landing page

--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "home/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.feature "Guest visiting landing page", :typ => :feature do
+  scenario "Guest visits" do
+    visit "/home/index"
+    
+    expect(page).to have_text("Welcome to the Digital Entertainment Tracker!")
+    expect(page).to have_table("",:text => "The End of the Big Cats")
+  end
 end

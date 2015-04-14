13f3b3ffbafde34ec3d69d3ab6dd95dc53c5c31b
Peter Munch-Ellingsen <peterme@peterme.net>
2015-04-14 02:38:51 +0200

    Fixed bug with landing page, list would stop populating after 10 entries was found (not show 10 upcoming as intended). Also bootstrapified the table

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,22 +4,25 @@ class HomeController < ApplicationController
     SeriesSubscription.find_each do |subscription|
       most_tracked_list[subscription.seriesid]=most_tracked_list[subscription.seriesid]+1
     end
-    series = most_tracked_list.sort_by{ |seriesid, count| count }.reverse
+    series = most_tracked_list.sort_by{ |seriesid, count| count }.reverse[0..7]
     client = TheTvDbParty::Client.new(ENV['TVDB_API_KEY'])
     @upcoming = Array.new
     i = 0
-    u = 0
+    #u = 0
     while i < series.length
       full_record = client.get_series_all(series[i][0]).full_series_record
       for j in 1..(full_record.episodes.length)
         if(full_record.episodes[full_record.episodes.length-j].firstaired)
           if(full_record.episodes[full_record.episodes.length-j].firstaired > Date.today)
-            @upcoming[u]=[full_record.seriesname,full_record.episodes[full_record.episodes.length-j].firstaired]
-            u+=1
-            if u==10
-              i = series.length
-              break
-            end
+            @upcoming.push([full_record.seriesname,
+                            full_record.episodes[full_record.episodes.length-j].firstaired,
+                            full_record.episodes[full_record.episodes.length-j].episodename,
+                            full_record.seriesid])
+            #u+=1
+            #if u==10
+            #  i = series.length
+            #  break
+            #end
           end
         end
       end
@@ -28,6 +31,7 @@ class HomeController < ApplicationController
     @upcoming.sort! { |x,y|
       x[1] <=> y[1]
     }
+    @upcoming = @upcoming[0..10]
   end
 
   def about

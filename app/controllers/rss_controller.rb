5102851f782b071a9cad3e9ee95f47f863f0259c
Alexander Nordvik <alextn89@gmail.com>
2015-05-15 18:39:13 +0200

    Added RSS Functions

--- app/controllers/rss_controller.rb
+++ app/controllers/rss_controller.rb
@@ -1,35 +1,45 @@
 class RssController < ApplicationController
-	def feed
-		most_tracked_list = Hash.new(0)
-	    SeriesSubscription.find_each do |subscription|
-	      most_tracked_list[subscription.seriesid]=most_tracked_list[subscription.seriesid]+1
-	    end
-	    series = most_tracked_list.sort_by{ |seriesid, count| count }.reverse[0..7]
-	    client = TheTvDbParty::Client.new(ENV['TVDB_API_KEY'])
-	    @upcoming = Array.new
-	    i = 0
-	    while i < series.length
-	      full_record = client.get_series_all(series[i][0]).full_series_record
-	      for j in 1..(full_record.episodes.length)
-	        if(full_record.episodes[full_record.episodes.length-j].firstaired)
-	          if(full_record.episodes[full_record.episodes.length-j].firstaired > Date.today)
-	            @upcoming.push([full_record.seriesname,
-	                            full_record.episodes[full_record.episodes.length-j].firstaired,
-	                            full_record.episodes[full_record.episodes.length-j].episodename,
-	                            full_record.seriesid])
-	          else
-	            break
-	          end
-	        end
-	      end
-	      i+=1
-	    end
-	    @upcoming.sort! { |x,y|
-	      x[1] <=> y[1]
-	    }
-	    @upcoming = @upcoming[0..10]
 
-	  end
+	def feed
+   @upcoming = Array.new
+    client = TheTvDbParty::Client.new(ENV['TVDB_API_KEY'])
+    series_subscriptions = current_user.series_subscriptions
+    for subscr in current_user.series_subscriptions.map
+      full_record = client.get_series_all(subscr.seriesid).full_series_record
+      for j in 1..(full_record.episodes.length)
+        if(full_record.episodes[full_record.episodes.length-j].firstaired)
+          if(full_record.episodes[full_record.episodes.length-j].firstaired > Date.today)
+          	episodename = full_record.episodes[full_record.episodes.length-j].episodename
+          
+          	if (!episodename)
+          		episodename = "TBA"
+          	end 
+          	overview = full_record.episodes[j].overview
+          	if (!overview)
+          		overview = "No released episode info!"
+          	end
+
+           @upcoming.push([full_record.seriesname,
+                            full_record.episodes[full_record.episodes.length-j].firstaired,
+                            episodename,
+                            full_record.seriesid,
+                            overview,
+                            full_record.episodes[j].imagepath_full])
+
+            				
+
+          else
+            break
+          end
+        end
+      end
+    end
+    @upcoming.sort! { |x,y|
+      x[1] <=> y[1]
+    }
+  end
 
 	
 end
+
+
--- app/views/rss/feed.rss.builder
+++ app/views/rss/feed.rss.builder
@@ -1,15 +1,34 @@
 xml.instruct! :xml, :version => "1.0"
 xml.rss :version => "2.0" do
   xml.channel do
-    xml.title "Series"
-    xml.description "Upcomming"
+    xml.title "Upcoming Series"
 
     for episode in @upcoming
+           xml.image do
+       xml.title          "lucashills.com"
+       xml.url            "https://pbs.twimg.com/profile_images/960696059/PIC_logo.jpg"
+       xml.link           "lucashills.com"
+       xml.width          "80"
+       xml.height         "37"
+       xml.description    "Random programming tips, notes and news I thought worth sharing"
+   end
+
       xml.item do
-        xml.title episode[0]
-        xml.episodeTitle episode[2]
-        xml.airDate episode[1]
+
+
+      xml.link url_for :only_path => false, :controller => 'series', :action => 'show', :seriesid => episode[3]
+      xml.title episode[0] + " - " + episode[2]
+      xml.pubDate episode[1]
+      xml.description episode[4]
+
+ 
+      
       end
     end
   end
 end
+
+
+
+
+

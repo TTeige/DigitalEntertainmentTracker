fd95ad167ba333d950d1f61daf629e58f85694a2
Peter Munch-Ellingsen <peterme@peterme.net>
2015-05-20 17:47:58 +0200

    Optimized the home page. It will now look on the metadata tracked instead of counting shows each time to find popular shows. It will also stop once it has more than ten shows from the most popular series.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,17 +1,16 @@
 class HomeController < ApplicationController
   def index
-    most_tracked_list = Hash.new(0)
-    SeriesSubscription.find_each do |subscription|
-      most_tracked_list[subscription.seriesid]=most_tracked_list[subscription.seriesid]+1
-    end
-    series = most_tracked_list.sort_by{ |seriesid, count| count }.reverse[0..7]
+    series = SeriesInformation.order("userssubscribed DESC").pluck(:seriesid)
     @upcoming = Array.new
     i = 0
     while i < series.length
-      seriesInformation = get_information_for_seriesid(series[i][0])
-      episodes = get_upcoming_episodes(series[i][0])
+      seriesInformation = get_information_for_seriesid(series[i])
+      episodes = get_upcoming_episodes(series[i])
       for episode in episodes
-        @upcoming.push([seriesInformation.seriesname,episode.firstaired,episode.episodename,series[i][0],episode.id])
+        @upcoming.push([seriesInformation.seriesname,episode.firstaired,episode.episodename,series[i],episode.id])
+      end
+      if(@upcoming.length>10)
+        break
       end
       i+=1
     end

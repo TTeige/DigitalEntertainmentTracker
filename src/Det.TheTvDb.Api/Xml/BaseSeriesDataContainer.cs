2708f2199b51931ce7922673cb4dc3cc67c295cf
TTeige <tim.teige91@gmail.com>
2015-11-19 19:57:40 +0100

    Added stub for BaseSeriesDataContainer

---
+++ src/Det.TheTvDb.Api/Xml/BaseSeriesDataContainer.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Det.TheTvDb.Api.Xml
+{
+    public class BaseSeriesDataContainer
+    {
+        public BaseSeriesDataContainer()
+        {
+        }
+    }
+}

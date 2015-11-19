9aa522662bb657238507c558f97a99a76809a53b
TTeige <tim.teige91@gmail.com>
2015-11-19 21:15:33 +0100

    Added base series xml

--- src/Det.TheTvDb.Api/Xml/BaseSeriesDataContainer.cs
+++ src/Det.TheTvDb.Api/Xml/BaseSeriesDataContainer.cs
@@ -2,13 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace Det.TheTvDb.Api.Xml
 {
+    [XmlRoot("Data")]
     public class BaseSeriesDataContainer
     {
-        public BaseSeriesDataContainer()
-        {
-        }
+        [XmlElement("Series")]
+        public BaseSeriesRecord[] BaseSeriesRecords { get; set; }
     }
 }
--- src/Det.TheTvDb.Api/Xml/BaseSeriesRecord.cs
+++ src/Det.TheTvDb.Api/Xml/BaseSeriesRecord.cs
@@ -2,13 +2,136 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace Det.TheTvDb.Api
 {
+    [XmlType]
     public class BaseSeriesRecord
     {
-        public BaseSeriesRecord()
+        private static readonly Uri mirrorPath = new Uri("http://thetvdb.com/api");
+
+        [XmlElement("seriesid")]
+        public uint SeriesId { get; set; }
+
+        [XmlElement("Actors")]
+        public string ActorsString { get; set; }
+
+        [XmlIgnore]
+        public string[] Actors
         {
+            get { return ActorsString?.Split(new char[] { '|' }); }
+            set { ActorsString = string.Join("|", value); }
         }
+
+        [XmlElement("Airs_DayOfWeek")]
+        public string AirsDayOfWeek { get; set; }
+
+        [XmlElement("Airs_Time")]
+        public string AirsTime { get; set; }
+
+        [XmlElement("ContentRating")]
+        public string ContentRating { get; set; }
+
+        [XmlElement("FirstAired")]
+        public DateTime FirstAired { get; set; }
+
+        [XmlElement("Genre")]
+        public string GenreString { get; set; }
+
+        [XmlIgnore]
+        public string[] Genre
+        {
+
+            get { return GenreString?.Split(new char[] { '|' }); }
+            set { ActorsString = string.Join("|", value); }
+        }
+
+        [XmlElement("IMDB_ID")]
+        public string IMDB_ID { get; set; }
+
+        [XmlElement("Language")]
+        public string Language { get; set; }
+
+        [XmlElement("Network")]
+        public string Network { get; set; }
+
+        [XmlElement("NetworkID")]
+        public uint NetworkID { get; set; }
+
+        [XmlElement("Rating")]
+        public float Rating { get; set; }
+
+        [XmlElement("RatingCount")]
+        public uint RatingCount { get; set; }
+
+        [XmlElement("Runtime")]
+        public uint Runtime { get; set; }
+
+        [XmlElement("SeriesID")]
+        public uint SeriesID { get; set; }
+
+        [XmlElement("SeriesName")]
+        public string SeriesName { get; set; }
+
+        [XmlElement("Status")]
+        public string Status { get; set; }
+
+        [XmlElement("added")]
+        public DateTime Added { get; set; }
+
+        [XmlElement("addedBy")]
+        public string AddedBy { get; set; }
+
+        [XmlElement("banner")]
+        public string BannerString { get; set; }
+
+        [XmlIgnore]
+        public Uri AbsoluteBannerPath
+        {
+            get { return new Uri(mirrorPath, BannerString); }
+        }
+
+        [XmlIgnore]
+        public Uri RelativeBannerPath
+        {
+            get { return new Uri(BannerString); }
+        }
+
+        [XmlElement("fanart")]
+        public string FanartString { get; set; }
+
+        [XmlIgnore]
+        public Uri AbsoluteFanartPath
+        {
+            get { return new Uri(mirrorPath, FanartString); }
+        }
+
+        [XmlIgnore]
+        public Uri RelativeFanartPath
+        {
+            get { return new Uri(FanartString); }
+        }
+
+        [XmlElement("lastupdated")]
+        public string LastUpdated { get; set; }
+
+        [XmlElement("posters")]
+        public string PosterString { get; set; }
+
+        [XmlIgnore]
+        public Uri AbsolutePosterPath
+        {
+            get { return new Uri(mirrorPath, PosterString); }
+        }
+
+        [XmlIgnore]
+        public Uri RelativePosterPath
+        {
+            get { return new Uri(PosterString); }
+        }
+
+        [XmlElement("zap2it_id")]
+        public string Zap2itId { get; set; }
     }
 }
